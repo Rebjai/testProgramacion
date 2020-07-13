4e81471e8f0ff4a0f700618cdfce89b2cc98eec0
rebjai <rebjai@github.com>
2020-07-13 10:30:58 -0500

    Added Models

---
+++ src/app/models/articulo.ts
@@ -0,0 +1,9 @@
+import { Categoria } from "./categoria";
+import { Precio } from "./precio";
+export class Articulo {
+    clave:string
+    categoria:Categoria
+    nombre:string
+    precios: Array<Precio> 
+    activo: boolean  = true
+}
---
+++ src/app/models/categoria.ts
@@ -0,0 +1,7 @@
+export class Categoria {
+    clave:string
+    fechaCreado: BigInteger 
+    nombre : string
+    activo: boolean = true
+
+}
